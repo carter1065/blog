4edbca03eef5d9dd7200778c60d2dd799acb0aa2
trinityvion <vion@tpng.team>
2025-07-12 19:11:10 -0400

    idk

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "My First Blog Post",
+        "excerpt":"I’m going to start writing here when my beautiful son Lucas is born.  ","categories": [],
+        "tags": [],
+        "url": "/2025/07/12/post.html",
+        "teaser": null
+      },{
+        "title": "My First Blog Post",
+        "excerpt":"I'm going to start writing here when my beautiful son Lucas is born.","categories": [],
+        "tags": [],
+        "url": "/2025/07/12/post.md.bak",
+        "teaser": null
+      }]
