3f1e82c10ce0b17b64be01292d4d4a3efdc22008
trinityvion <vion@tpng.team>
2025-07-20 10:27:33 -0400

    sgd

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": [],
         "url": "/2025/07/12/post.html",
         "teaser": null
+      },{
+        "title": "Autism isn't real",
+        "excerpt":"Autism is not real. There is no coherent argument that can be made for the existence of autism. It is impossible to even construct a mental model of a person who actually knows what autism “is,” and yet who genuinely believes that it exists. At best, there are people who...","categories": [],
+        "tags": [],
+        "url": "/2025/07/19/Autism-isn't-real.html",
+        "teaser": null
       }]
